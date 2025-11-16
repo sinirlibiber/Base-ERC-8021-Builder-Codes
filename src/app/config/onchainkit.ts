1a0bfa04027198092c1872fe7f8521da52a4d537
gumusbey <98549582+sinirlibiber@users.noreply.github.com>
2025-11-16 15:56:58 +0300

    Create onchainkit.ts

---
+++ src/app/config/onchainkit.ts
@@ -0,0 +1,2 @@
+export const ONCHAINKIT_API_KEY = process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY || 'fdabc0d7-0feb-4eb5-97f4-1ccf1fc46c60';
+export const ONCHAINKIT_PROJECT_ID = process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_ID || 'cffc9dae-3cf3-49eb-b1a5-89ce86c53d1a';
